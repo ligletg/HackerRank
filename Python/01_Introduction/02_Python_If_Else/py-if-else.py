e4f43e637b6fb0db54c00b4b3e946bba087a59d4
Gregoire Liglet <gregoire.liglet@gmail.com>
2018-05-02 17:34:08 -0700

    adding if-else python exo

---
+++ Python/01_Introduction/02_Python_If_Else/py-if-else.py
@@ -0,0 +1,20 @@
+def main():
+    n = input()
+
+    if n % 2:
+        print "Weird"
+        return
+
+    if n >= 2 and n <= 5:
+        print "Not Weird"
+        return
+
+    if n >= 6 and n <= 20:
+        print "Weird"
+
+    if n > 20:
+        print "Not Weird"
+
+
+if __name__ == '__main__':
+    main()
